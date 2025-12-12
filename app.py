b4d50cd1f7c7b50e3af15678543c48764fb4b646
minhanhnb <121483421+minhanhnb@users.noreply.github.com>
2025-12-12 14:20:23 +0700

    change to apply deployment

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import os, json
+import firebase_admin
+from firebase_admin import credentials
 from config import Config
 from firebase_config import db  # Import db from firebase_config
 
 app = Flask(__name__)
-app.config.from_object(Config)
 
 # # Import and register blueprints
 from routes.home import home_bp
@@ -18,6 +20,16 @@ app.register_blueprint(ttt_bp)
 from routes.system_routes import system_bp
 
 app.register_blueprint(system_bp)
+# Khởi tạo Firebase 1 lần khi app khởi động
+firebase_json = os.environ.get('FIREBASE_JSON')
+if firebase_json:
+    cred_dict = json.loads(firebase_json)
+    cred = credentials.Certificate(cred_dict)
+    try:
+        firebase_admin.initialize_app(cred)
+    except ValueError:
+        # đã init trước đó thì bỏ qua
+        pass
 @app.route("/test")
 def test_firebase():
     doc_ref = db.collection("test_collection").document("demo_doc")
